8eb5721c35466111635b98c22cde72406e31b3f5
Stefan Siegl <stesie@brokenpipe.de>
2013-10-19 02:52:19 +0200

    Copy default_properties to new object (PHP 5.3)

--- v8js.cc
+++ v8js.cc
@@ -536,6 +536,10 @@ static zend_object_value php_v8js_new(zend_class_entry *ce TSRMLS_DC) /* {{{ */
 	zend_object_std_init(&c->std, ce TSRMLS_CC);
 #if PHP_VERSION_ID >= 50400
 	object_properties_init(&c->std, ce);
+#else
+	zval *tmp;
+	zend_hash_copy(c->std.properties, &ce->default_properties,
+				   (copy_ctor_func_t) zval_add_ref, (void *) &tmp, sizeof(zval *));
 #endif
 
 	retval.handle = zend_objects_store_put(c, NULL, (zend_objects_free_object_storage_t) php_v8js_free_storage, NULL TSRMLS_CC);
