b21acf770aa8c1cb4211c7af872c61960222b613
Stefan Siegl <stesie@brokenpipe.de>
2015-08-29 20:19:44 +0200

    PHP7 adapt: no fallback property_info any longer

--- v8js_class.cc
+++ v8js_class.cc
@@ -992,7 +992,7 @@ static void v8js_write_property(zval *object, zval *member, zval *value, void **
 
 	/* Check whether member is public, if so, export to V8. */
 	zend_property_info *property_info = zend_get_property_info(c->std.ce, Z_STR_P(member), 1 TSRMLS_CC);
-	if(property_info->flags & ZEND_ACC_PUBLIC) {
+	if(!property_info || property_info->flags & ZEND_ACC_PUBLIC) {
 		/* Global PHP JS object */
 		v8::Local<v8::String> object_name_js = v8::Local<v8::String>::New(isolate, c->object_name);
 		v8::Local<v8::Object> jsobj = V8JS_GLOBAL(isolate)->Get(object_name_js)->ToObject();
