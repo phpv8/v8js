90b2b2b0b1c011b4f43794d2a0e712debc35d3dd
Stefan Siegl <stesie@brokenpipe.de>
2016-03-24 12:28:00 +0100

    Bump version to 0.6.1

--- php_v8js_macros.h
+++ php_v8js_macros.h
@@ -53,7 +53,7 @@ extern "C" {
 #endif
 
 /* V8Js Version */
-#define PHP_V8JS_VERSION "0.6.0"
+#define PHP_V8JS_VERSION "0.6.1"
 
 /* Hidden field name used to link JS wrappers with underlying PHP object */
 #define PHPJS_OBJECT_KEY "phpjs::object"
