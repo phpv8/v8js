18b79d90046a63209cedf58ef24e2f4f0281f7a1
Stefan Siegl <stesie@brokenpipe.de>
2018-07-18 23:36:13 +0200

    don't free interned strings, only persistent ones

--- v8js_class.cc
+++ v8js_class.cc
@@ -991,7 +991,10 @@ static void v8js_persistent_zval_ctor(zval *p) /* {{{ */
 static void v8js_persistent_zval_dtor(zval *p) /* {{{ */
 {
 	assert(Z_TYPE_P(p) == IS_STRING);
-	free(Z_STR_P(p));
+
+	if (!ZSTR_IS_INTERNED(Z_STR_P(p))) {
+		free(Z_STR_P(p));
+	}
 }
 /* }}} */
 
