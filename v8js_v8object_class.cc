c2873fd8d39c5b0f7507468bd0f17cbc0e45558e
Stefan Siegl <stesie@brokenpipe.de>
2019-06-22 20:25:54 +0200

    change retval of write_property handlers, refs #409

--- v8js_v8object_class.cc
+++ v8js_v8object_class.cc
@@ -161,29 +161,31 @@ static zval *v8js_v8object_read_property(zval *object, zval *member, int type, v
 }
 /* }}} */
 
-static void v8js_v8object_write_property(zval *object, zval *member, zval *value, void **cache_slot ) /* {{{ */
+static zval* v8js_v8object_write_property(zval *object, zval *member, zval *value, void **cache_slot ) /* {{{ */
 {
 	v8js_v8object *obj = Z_V8JS_V8OBJECT_OBJ_P(object);
 
 	if (!obj->ctx) {
 		zend_throw_exception(php_ce_v8js_exception,
 			"Can't access V8Object after V8Js instance is destroyed!", 0);
-		return;
+		return value;
 	}
 
-	V8JS_CTX_PROLOGUE(obj->ctx);
+	V8JS_CTX_PROLOGUE_EX(obj->ctx, value);
 	v8::Local<v8::Value> v8objHandle = v8::Local<v8::Value>::New(isolate, obj->v8obj);
 
 	if (Z_STRLEN_P(member) > std::numeric_limits<int>::max()) {
 		zend_throw_exception(php_ce_v8js_exception,
 			"Member name length exceeds maximum supported length", 0);
-		return;
+		return value;
 	}
 
 	v8::Local<v8::Object> v8obj;
 	if (v8objHandle->IsObject() && v8objHandle->ToObject(v8_context).ToLocal(&v8obj)) {
 		v8obj->CreateDataProperty(v8_context, V8JS_ZSYM(Z_STR_P(member)), zval_to_v8js(value, isolate));
 	}
+
+	return value;
 }
 /* }}} */
 
